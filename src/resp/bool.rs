06dcd07ac7ca970b4373f5300b240a0c56ea41b8
forfd8960 <forfd8960@gmail.com>
2024-08-13 23:27:27 +0800

    feat: Encode & Decode for bool

--- src/resp/bool.rs
+++ src/resp/bool.rs
@@ -1,2 +1,32 @@
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd)]
-pub struct RespBool(bool);
+use super::{extract_fixed_data, RespDecode, RespEncode, RespError};
+
+// #<t|f>\r\n
+// https://redis.io/docs/latest/develop/reference/protocol-spec/#booleans
+impl RespEncode for bool {
+    fn encode(self) -> Vec<u8> {
+        if self {
+            b"#t\r\n".to_vec()
+        } else {
+            b"#f\r\n".to_vec()
+        }
+    }
+}
+
+impl RespDecode for bool {
+    const PREFIX: &'static str = "#";
+    fn decode(buf: &mut bytes::BytesMut) -> Result<Self, RespError> {
+        match extract_fixed_data(buf, "#t\r\n", "Bool") {
+            Ok(_) => Ok(true),
+            Err(RespError::NotComplete) => Err(RespError::NotComplete),
+
+            Err(_) => match extract_fixed_data(buf, "#f\r\n", "Bool") {
+                Ok(_) => Ok(false),
+                Err(e) => Err(e),
+            },
+        }
+    }
+
+    fn expect_length(_buf: &[u8]) -> Result<usize, RespError> {
+        Ok(4)
+    }
+}
