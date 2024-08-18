a21e0cce6f676b9c9660796ef064a59b4b61b93d
forfd8960 <forfd8960@gmail.com>
2024-08-18 09:21:08 +0800

    feat: network - stream_handler

--- src/network.rs
+++ src/network.rs
@@ -1,7 +1,13 @@
+use anyhow::Result;
 use bytes::BytesMut;
-use tokio_util::codec::{Decoder, Encoder};
+use futures::SinkExt;
+use tokio::net::TcpStream;
+use tokio_stream::StreamExt;
+use tokio_util::codec::{Decoder, Encoder, Framed};
+use tracing::info;
 
 use crate::{
+    command::{Command, CommandExecutor},
     resp::{RespDecode, RespEncode, RespError, RespFrame},
     storage::memory::InMemStore,
 };
@@ -18,6 +24,38 @@ struct RedisRequest {
 #[derive(Debug)]
 struct RedisResponse {
     frame: RespFrame,
+}
+
+pub async fn stream_handler(stream: TcpStream, store: InMemStore) -> Result<()> {
+    let mut framed = Framed::new(stream, RespFrameCodec);
+
+    loop {
+        match framed.next().await {
+            Some(Ok(frame)) => {
+                info!("received frame: {:?}", frame);
+                let request = RedisRequest {
+                    frame,
+                    store: store.clone(),
+                };
+
+                let response = handle_request(request).await?;
+
+                info!("sending response: {:?}", response.frame);
+                framed.send(response.frame).await?;
+            }
+            Some(Err(e)) => return Err(e),
+            None => return Ok(()),
+        }
+    }
+}
+
+async fn handle_request(request: RedisRequest) -> Result<RedisResponse> {
+    let (frame, store) = (request.frame, request.store);
+    let cmd = Command::try_from(frame)?;
+    info!("Execute command: {:?}", cmd);
+
+    let resp_frame = cmd.execute(&store);
+    Ok(RedisResponse { frame: resp_frame })
 }
 
 impl Encoder<RespFrame> for RespFrameCodec {
