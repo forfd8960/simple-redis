41005bf82972d4a7aeaf78ba421a0de1c892daf6
forfd8960 <forfd8960@gmail.com>
2024-08-18 11:40:49 +0800

    feat: implment main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use simple_redis::{network, storage::memory::InMemStore};
+use tokio::net::TcpListener;
+use tracing::{info, warn};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let addr = "0.0.0.0:8088";
+    info!("listen on: {}", addr);
+    let listener = TcpListener::bind(addr).await?;
+
+    let store = InMemStore::new();
+    loop {
+        let (stream, raddr) = listener.accept().await?;
+        info!("accept connection from: {}", raddr);
+
+        let inner_store = store.clone();
+
+        tokio::spawn(async move {
+            match network::stream_handler(stream, inner_store).await {
+                Ok(_) => {
+                    info!("connection from {} exit", raddr);
+                }
+                Err(e) => {
+                    warn!("handle error for {}:{}", raddr, e);
+                }
+            }
+        });
+    }
 }
